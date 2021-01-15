326313b4c7a7d9ae4d7baad0d7d7e3ad1dd277cf
Axel <axosar@gmail.com>
2021-01-15 09:26:54 -0600

    Function export ex

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import location, {message, name, getGreeting} from './myModule'
+import add from './math'
 
 console.log(message);
 console.log(name)
 console.log(location);
+console.log(add(2, 5))
 console.log(getGreeting(name))
